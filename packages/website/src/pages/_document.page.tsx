5755a1e2c0bdaa29719786c27aa3f105ea49d244
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2025-01-19 13:32:58 +0000

    Add GTM

--- packages/website/src/pages/_document.page.tsx
+++ packages/website/src/pages/_document.page.tsx
@@ -18,6 +18,15 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://${process.env.GTM_URL}/metrics'+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+            }}
+          />
         </Head>
         <body className='bg-pageBackground'>
           <Main />
