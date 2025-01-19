0f0cae14f6c9193d2388be97c67195c6a3fc7e52
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2025-01-19 13:36:16 +0000

    Relative GTM URL

--- packages/website/src/pages/_document.page.tsx
+++ packages/website/src/pages/_document.page.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://${process.env.GTM_URL}/metrics'+i+dl;f.parentNode.insertBefore(j,f);
+              '/metrics'+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
             }}
           />
