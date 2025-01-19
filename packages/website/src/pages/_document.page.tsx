b18629408506e07da7d25536158043b6c4ce46a1
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2025-01-19 20:54:40 +0000

    Specify GTM url domain

--- packages/website/src/pages/_document.page.tsx
+++ packages/website/src/pages/_document.page.tsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              '/metrics/?id='+i+dl;f.parentNode.insertBefore(j,f);
+              'https://demo.primecutsites.com/metrics/?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
             }}
           />
