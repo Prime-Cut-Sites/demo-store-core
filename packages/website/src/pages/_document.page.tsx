b1fa3a27873c37968110888b0a6bf92019a6ddd7
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2025-01-19 13:52:00 +0000

    Update GTM snippet

--- packages/website/src/pages/_document.page.tsx
+++ packages/website/src/pages/_document.page.tsx
@@ -23,8 +23,8 @@ class MyDocument extends Document {
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              '/metrics'+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+              '/metrics/?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','');`,
             }}
           />
         </Head>
